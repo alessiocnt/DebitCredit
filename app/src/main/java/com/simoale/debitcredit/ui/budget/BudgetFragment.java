0a239d375d8b19ea7a710a66cf5221630d475fe7
Alessio Conti <alessio.conti3@studio.unibo.it>
2021-05-05 14:24:23 +0200

    Card budget

--- app/src/main/java/com/simoale/debitcredit/ui/budget/BudgetFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/budget/BudgetFragment.java
@@ -1,6 +1,22 @@
 package com.simoale.debitcredit.ui.budget;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class BudgetFragment extends Activity {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.simoale.debitcredit.R;
+
+public class BudgetFragment extends Fragment {
+
+    private View view;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        this.view = inflater.inflate(R.layout.fragment_budget, container, false);
+        return this.view;
+    }
 }
