f32bddd13f10fcbbc4b2b515c38c17a360911472
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-07 18:01:55 +0200

    Created wallet details fragment

--- app/src/main/java/com/simoale/debitcredit/ui/wallet/WalletFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/wallet/WalletFragment.java
@@ -6,9 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.simoale.debitcredit.R;
 
 public class WalletFragment extends Fragment {
@@ -21,5 +24,17 @@ public class WalletFragment extends Fragment {
                 new ViewModelProvider(this).get(WalletViewModel.class);
         View root = inflater.inflate(R.layout.fragment_wallet, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FloatingActionButton fab = view.findViewById(R.id.fab_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_walletFragment_to_walletDetailsFragment);
+            }
+        });
     }
 }
