2d922f3edc78c69c07cf31c5bf2612903df1cf97
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-07 09:50:34 +0200

    Fix inflated layout

--- app/src/main/java/com/simoale/debitcredit/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/settings/SettingsFragment.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.simoale.debitcredit.R;
 
@@ -20,9 +16,9 @@ public class SettingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+//        settingsViewModel =
+//                new ViewModelProvider(this).get(SettingsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
         /**final TextView textView = root.findViewById(R.id.text_home);
         settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
