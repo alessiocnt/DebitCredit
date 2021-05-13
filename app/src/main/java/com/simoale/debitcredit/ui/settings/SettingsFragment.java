73b33003676c6492c56e73c5d9cb6127f4cfadeb
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-13 12:24:30 +0200

    Created setting layout

--- app/src/main/java/com/simoale/debitcredit/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/settings/SettingsFragment.java
@@ -1,31 +1,25 @@
 package com.simoale.debitcredit.ui.settings;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.simoale.debitcredit.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    private SettingsViewModel settingsViewModel;
+//    private SettingsViewModel settingsViewModel;
+//
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//
+//        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+//
+//        return root;
+//    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-//        settingsViewModel =
-//                new ViewModelProvider(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        /**final TextView textView = root.findViewById(R.id.text_home);
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });**/
-        return root;
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_layout, rootKey);
     }
 }
---
+++ app/src/main/java/com/simoale/debitcredit/utils/Utilities.java
@@ -0,0 +1,47 @@
+package com.simoale.debitcredit.utils;
+
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+
+import java.io.InputStream;
+
+public class Utilities {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    
+
+    public static Bitmap getImageBitmap(Activity activity, Uri currentPhotoUri) {
+        ContentResolver resolver = activity.getApplicationContext()
+                .getContentResolver();
+        try {
+            InputStream stream = resolver.openInputStream(currentPhotoUri);
+            Bitmap bitmap = BitmapFactory.decodeStream(stream);
+            stream.close();
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+}
