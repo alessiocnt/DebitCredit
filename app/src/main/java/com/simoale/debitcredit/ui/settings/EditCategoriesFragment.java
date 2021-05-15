c7ed9c60458df2ba8a9bb9bad13858340123d7ad
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-15 11:44:30 +0200

    Changed Settings fragment and sub fragments

---
+++ app/src/main/java/com/simoale/debitcredit/ui/settings/EditCategoriesFragment.java
@@ -0,0 +1,20 @@
+package com.simoale.debitcredit.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.simoale.debitcredit.R;
+
+public class EditCategoriesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.edit_categories, container, false);
+    }
+}
--- app/src/main/java/com/simoale/debitcredit/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/settings/SettingsFragment.java
@@ -1,25 +1,40 @@
 package com.simoale.debitcredit.ui.settings;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import androidx.preference.PreferenceFragmentCompat;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.card.MaterialCardView;
 import com.simoale.debitcredit.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends Fragment {
 
-//    private SettingsViewModel settingsViewModel;
-//
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-//
-//        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-//
-//        return root;
-//    }
+    private SettingsViewModel settingsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return root;
+    }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.settings_layout, rootKey);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MaterialCardView changePwdBtn = view.findViewById(R.id.change_password_button);
+        changePwdBtn.setOnClickListener((v) -> Navigation.findNavController(v).navigate(R.id.action_nav_settings_to_changePasswordFragment));
+        MaterialCardView editWalletsBtn = view.findViewById(R.id.edit_wallets_button);
+        editWalletsBtn.setOnClickListener((v) -> Navigation.findNavController(v).navigate(R.id.action_nav_settings_to_editWalletsFragment));
+        MaterialCardView editCatsBtn = view.findViewById(R.id.edit_categories_button);
+        editCatsBtn.setOnClickListener((v) -> Navigation.findNavController(v).navigate(R.id.action_nav_settings_to_editCategoriesFragment));
+        MaterialCardView editTagsBtn = view.findViewById(R.id.edit_tags_button);
+        editTagsBtn.setOnClickListener((v) -> Navigation.findNavController(v).navigate(R.id.action_nav_settings_to_editTagsFragment));
     }
 }
