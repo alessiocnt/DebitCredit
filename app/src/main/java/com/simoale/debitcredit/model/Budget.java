b6bc5ae72a2f1f4ea667e982fb39e743536f8589
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-19 10:12:32 +0200

    Added year to Interval

--- app/src/main/java/com/simoale/debitcredit/model/Budget.java
+++ app/src/main/java/com/simoale/debitcredit/model/Budget.java
@@ -124,7 +124,8 @@ public class Budget {
     public enum Interval {
         DAY(1),
         WEEK(7),
-        MONTH(31);
+        MONTH(31),
+        YEAR(365);
         public final int daysNumber;
 
         private Interval(int daysNumber) {
