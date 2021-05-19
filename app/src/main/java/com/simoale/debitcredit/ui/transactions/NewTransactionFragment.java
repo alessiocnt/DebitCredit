e218c9fd6b24ef64be5743227aa660579eed00e8
Alessio Conti <alessio.conti3@studio.unibo.it>
2021-05-19 10:42:59 +0200

    Date picker in new transaction

--- app/src/main/java/com/simoale/debitcredit/ui/transactions/NewTransactionFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/transactions/NewTransactionFragment.java
@@ -1,24 +1,112 @@
 package com.simoale.debitcredit.ui.transactions;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.simoale.debitcredit.R;
+import com.simoale.debitcredit.model.Transaction;
+import com.simoale.debitcredit.model.Wallet;
+import com.simoale.debitcredit.ui.wallet.WalletViewModel;
+
+import java.util.Calendar;
 
 public class NewTransactionFragment extends Fragment {
+
+    private Activity activity;
+    private Button saveBtn;
+    private Button cancelBtn;
+    private TransactionViewModel transactionViewModel;
 
     public NewTransactionFragment() {}
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.new_transaction, container, false);
-
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.activity = getActivity();
+
+        if (activity != null) {
+            this.saveBtn = getView().findViewById(R.id.transaction_save_button);
+            this.cancelBtn = getView().findViewById(R.id.transaction_cancel_button);
+            this.transactionViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(TransactionViewModel.class);
+            
+            setupDatePicker();
+            
+            
+            /*this.saveBtn.setOnClickListener(v -> {
+                retriveData();
+
+                TextInputLayout walletNameEditText = activity.findViewById(R.id.wallet_name_TextInput);
+                String walletName = walletNameEditText.getEditText().getText().toString();
+                TextInputLayout walletAmountEditText = activity.findViewById(R.id.wallet_initial_value);
+                String walletAmount = walletAmountEditText.getEditText().getText().toString();
+                this.walletViewModel.addWallet(new Wallet(walletName, "nice", Integer.parseInt(walletAmount), "null"));
+            });*/
+            
+        }
+    }
+
+    private void setupDatePicker() {
+        ImageButton calendar = getView().findViewById(R.id.calendar);
+        calendar.setOnClickListener(v -> {
+            DialogFragment newFragment = new DatePickerFragment();
+            newFragment.show(requireActivity().getSupportFragmentManager(), "datePicker");
+        });
+    }
+
+    private Transaction retriveData() {
+        TextInputLayout amountEditText = activity.findViewById(R.id.transaction_amount_InputEditText);
+        String amount = amountEditText.getEditText().getText().toString();
+
+        TextInputLayout descriptionEditText = activity.findViewById(R.id.transaction_description_InputEditText);
+        String description = descriptionEditText.getEditText().getText().toString();
+
+        TextInputLayout categoryEditText = activity.findViewById(R.id.transaction_category_InputEditText);
+        String category = categoryEditText.getEditText().getText().toString();
+
+        TextInputLayout payeeEditText = activity.findViewById(R.id.transaction_payee_InputEditText);
+        String payee = payeeEditText.getEditText().getText().toString();
+
+        return null;
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            TextView dateDisplay = getActivity().findViewById(R.id.date_display);
+            dateDisplay.setText("Date: " + day + "/" + month + "/" + year);
+        }
     }
 }
