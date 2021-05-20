4b9b4ec3e44d3eafd396e78b1b2ccd13e983098d
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-20 17:19:30 +0200

    Added color to wallet card

--- app/src/main/java/com/simoale/debitcredit/recyclerView/WalletCardAdapter.java
+++ app/src/main/java/com/simoale/debitcredit/recyclerView/WalletCardAdapter.java
@@ -1,25 +1,16 @@
 package com.simoale.debitcredit.recyclerView;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.simoale.debitcredit.R;
 import com.simoale.debitcredit.model.Wallet;
 import com.simoale.debitcredit.ui.wallet.WalletCardViewHolder;
-import com.simoale.debitcredit.ui.wallet.WalletViewModel;
-import com.simoale.debitcredit.utils.Utilities;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +30,9 @@ public class WalletCardAdapter extends RecyclerView.Adapter<WalletCardViewHolder
         this.listener = listener;
     }
 
-    /** Called when RecyclerView needs a new RecyclerView.ViewHolder of the given type to represent an item. */
+    /**
+     * Called when RecyclerView needs a new RecyclerView.ViewHolder of the given type to represent an item.
+     */
     @NonNull
     @Override
     public WalletCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -48,27 +41,17 @@ public class WalletCardAdapter extends RecyclerView.Adapter<WalletCardViewHolder
         return new WalletCardViewHolder(layoutView, listener);
     }
 
-    /** Called by RecyclerView to display the data at the specified position.
+    /**
+     * Called by RecyclerView to display the data at the specified position.
      * This method should update the contents of the RecyclerView.ViewHolder.itemView to reflect
      * the item at the given position.
      */
     @Override
     public void onBindViewHolder(@NonNull WalletCardViewHolder holder, int position) {
         Wallet currentWallet = walletList.get(position);
-        /*
-        String image_path = currentWallet.getImage();
-        if (image_path.contains("ic_")) {
-            Drawable drawable = ContextCompat.getDrawable(activity, activity.getResources()
-                    .getIdentifier(image_path, "drawable",
-                            activity.getPackageName()));
-            holder.getImage().setImageDrawable(drawable);
-        } else {
-            Bitmap bitmap = Utilities.getImageBitmap(activity, Uri.parse(image_path));
-            if (bitmap != null){
-                holder.getImage().setImageBitmap(bitmap);
-            }
-        }
-*/
+
+        String icon_color = currentWallet.getImage();
+        holder.getImage().getDrawable().setTint(Integer.valueOf(icon_color));
         holder.getName().setText(currentWallet.getName());
         holder.getBalance().setText(String.valueOf(currentWallet.getBalance()));
     }
