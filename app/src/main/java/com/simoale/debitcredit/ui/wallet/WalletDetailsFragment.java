b75ec7341f9257764962b1fa224a2858490b2e58
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-21 10:35:46 +0200

    Added wallet info to walletdetails

--- app/src/main/java/com/simoale/debitcredit/ui/wallet/WalletDetailsFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/wallet/WalletDetailsFragment.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.simoale.debitcredit.R;
 import com.simoale.debitcredit.model.Transaction;
+import com.simoale.debitcredit.model.Wallet;
 import com.simoale.debitcredit.recyclerView.OnItemListener;
 import com.simoale.debitcredit.recyclerView.TransactionCardAdapter;
 import com.simoale.debitcredit.ui.transactions.TransactionViewModel;
@@ -34,6 +37,8 @@ public class WalletDetailsFragment extends Fragment implements OnItemListener {
     private WalletViewModel walletViewModel;
     private TransactionViewModel transactionViewModel;
     private RecyclerView recyclerView;
+
+    private Wallet wallet;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -50,8 +55,16 @@ public class WalletDetailsFragment extends Fragment implements OnItemListener {
         final Activity activity = getActivity();
         if (activity != null) {
             setRecyclerView(activity);
-
+            ImageView walletIcon = view.findViewById(R.id.wallet_details_image);
+            TextView walletName = view.findViewById(R.id.wallet_details_name);
+            TextView walletBalance = view.findViewById(R.id.wallet_details_balance);
+            TextView walletDescription = view.findViewById(R.id.wallet_details_description);
             walletViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(WalletViewModel.class);
+            this.wallet = walletViewModel.getSelected().getValue();
+            walletIcon.getDrawable().setTint(Integer.parseInt(this.wallet.getImage()));
+            walletName.setText(this.wallet.getName());
+            walletBalance.setText(this.wallet.getBalance() + "€");
+            walletDescription.setText(this.wallet.getDescription());
             transactionViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(TransactionViewModel.class);
             // TODO selezionare solo la lista del wallet interessato che si trova in walletViewModel.getSelected()
             transactionViewModel.getTransactionList().observe((LifecycleOwner) activity, new Observer<List<Transaction>>() {
@@ -63,7 +76,6 @@ public class WalletDetailsFragment extends Fragment implements OnItemListener {
         } else {
             Log.e(LOG, "Activity is null");
         }
-
     }
 
     // // Set up the RecyclerView
