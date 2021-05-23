b42968f64f9cac376f6b8fe7668713e67d2a2b3e
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-23 09:37:07 +0200

    Fix cross refs

--- app/src/main/java/com/simoale/debitcredit/model/RoutineTagCrossRef.java
+++ app/src/main/java/com/simoale/debitcredit/model/RoutineTagCrossRef.java
@@ -4,27 +4,32 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 
+import org.jetbrains.annotations.NotNull;
+
 import static androidx.room.ForeignKey.CASCADE;
 import static androidx.room.ForeignKey.RESTRICT;
 
-@Entity(tableName="routine_tag",
-    primaryKeys = {"routine_id, tag_id"},
-    foreignKeys = {
-        @ForeignKey(entity = Transaction.class,
-                parentColumns = "routine_id",
-                childColumns = "routine_id",
-                onDelete = RESTRICT,
-                onUpdate = CASCADE),
-        @ForeignKey(entity = Tag.class,
-                parentColumns = "tag_id",
-                childColumns = "tag_id",
-                onDelete = RESTRICT,
-                onUpdate = CASCADE)
-    })
+@Entity(tableName = "routine_tag",
+        primaryKeys = {"routine_id", "tag_name"},
+        foreignKeys = {
+                @ForeignKey(entity = Routine.class,
+                        parentColumns = "routine_id",
+                        childColumns = "routine_id",
+                        onDelete = RESTRICT,
+                        onUpdate = CASCADE),
+                @ForeignKey(entity = Tag.class,
+                        parentColumns = "tag_name",
+                        childColumns = "tag_name",
+                        onDelete = RESTRICT,
+                        onUpdate = CASCADE)
+        })
+
 public class RoutineTagCrossRef {
 
     @ColumnInfo(name = "routine_id")
     public int routineId;
-    @ColumnInfo(name = "tag_id")
-    public int tagId;
+
+    @NotNull
+    @ColumnInfo(name = "tag_name")
+    public String tagName;
 }
--- app/src/main/java/com/simoale/debitcredit/model/TransactionTagCrossRef.java
+++ app/src/main/java/com/simoale/debitcredit/model/TransactionTagCrossRef.java
@@ -4,27 +4,31 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 
+import org.jetbrains.annotations.NotNull;
+
 import static androidx.room.ForeignKey.CASCADE;
 import static androidx.room.ForeignKey.RESTRICT;
 
-@Entity(tableName="transaction_tag",
-    primaryKeys = {"transaction_id, tag_id"},
-    foreignKeys = {
-        @ForeignKey(entity = Transaction.class,
-                parentColumns = "transaction_id",
-                childColumns = "transaction_id",
-                onDelete = RESTRICT,
-                onUpdate = CASCADE),
-        @ForeignKey(entity = Tag.class,
-                parentColumns = "tag_id",
-                childColumns = "tag_id",
-                onDelete = RESTRICT,
-                onUpdate = CASCADE)
-    })
+@Entity(tableName = "transaction_tag",
+        primaryKeys = {"cr_transaction_id", "cr_tag_name"},
+        foreignKeys = {
+                @ForeignKey(entity = Transaction.class,
+                        parentColumns = "transaction_id",
+                        childColumns = "cr_transaction_id",
+                        onDelete = RESTRICT,
+                        onUpdate = CASCADE),
+                @ForeignKey(entity = Tag.class,
+                        parentColumns = "tag_name",
+                        childColumns = "cr_tag_name",
+                        onDelete = RESTRICT,
+                        onUpdate = CASCADE)
+        })
 public class TransactionTagCrossRef {
 
-    @ColumnInfo(name = "transaction_id")
-    public int transactionId;
-    @ColumnInfo(name = "tag_id")
-    public int tagId;
+    @ColumnInfo(name = "cr_transaction_id")
+    public int crTransactionId;
+
+    @NotNull
+    @ColumnInfo(name = "cr_tag_name")
+    public String crTagName;
 }
