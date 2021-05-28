9afdb9d0be44b8f463ac6fd7803cb087e3e952aa
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-28 11:15:24 +0200

    Fixed graphics dates order

--- app/src/main/java/com/simoale/debitcredit/ui/graphs/GraphsFragment.java
+++ app/src/main/java/com/simoale/debitcredit/ui/graphs/GraphsFragment.java
@@ -3,65 +3,44 @@ package com.simoale.debitcredit.ui.graphs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.navigation.Navigation;
 
 import com.anychart.APIlib;
-import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Cartesian;
-import com.anychart.core.cartesian.series.Column;
-import com.anychart.core.cartesian.series.Line;
-import com.anychart.data.Mapping;
-import com.anychart.data.Set;
-import com.anychart.enums.Anchor;
-import com.anychart.enums.HoverMode;
-import com.anychart.enums.MarkerType;
-import com.anychart.enums.Position;
-import com.anychart.enums.TooltipPositionMode;
-import com.anychart.graphics.vector.Stroke;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 import com.simoale.debitcredit.R;
 import com.simoale.debitcredit.model.Category;
-import com.simoale.debitcredit.model.Tag;
 import com.simoale.debitcredit.model.Transaction;
 import com.simoale.debitcredit.model.Wallet;
 import com.simoale.debitcredit.ui.category.CategoryViewModel;
-import com.simoale.debitcredit.ui.tag.TagViewModel;
 import com.simoale.debitcredit.ui.transactions.TransactionViewModel;
 import com.simoale.debitcredit.ui.wallet.WalletViewModel;
 import com.simoale.debitcredit.utils.DatePicker;
 import com.simoale.debitcredit.utils.Utilities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class GraphsFragment extends Fragment {
     private static final String LOG = "Graphs-Fragment_SIMOALE";
@@ -78,12 +57,13 @@ public class GraphsFragment extends Fragment {
     private String dateSelectedTo;
 
     private Button applyBtn;
+    private Button deleteBtn;
     private TextView dateDisplayFrom;
     private TextView dateDisplayTo;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-       // graphsViewModel = new ViewModelProvider(this).get(GraphsViewModel.class);
+        // graphsViewModel = new ViewModelProvider(this).get(GraphsViewModel.class);
         this.view = inflater.inflate(R.layout.fragment_graphs, container, false);
         return this.view;
     }
@@ -104,20 +84,26 @@ public class GraphsFragment extends Fragment {
 
             this.applyBtn.setOnClickListener(v -> {
                 if (Utilities.checkDataValid(dateSelectedFrom, dateSelectedTo)) {
+                    this.applyBtn.setVisibility(View.INVISIBLE);
+                    this.deleteBtn.setVisibility(View.VISIBLE);
                     this.computeFilters();
                 } else {
                     Toast.makeText(activity.getBaseContext(), "Select dates to analise a period", Toast.LENGTH_LONG).show();
                 }
             });
+
+            this.deleteBtn.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_nav_graphs_self));
+
         } else {
             Log.e(LOG, "Activity is null");
         }
     }
 
     public void setupUi() {
-        this.dateDisplayFrom = activity.findViewById(R.id.graphs_date_from_date);
-        this.dateDisplayTo = activity.findViewById(R.id.graphs_date_to_date);
+        this.dateDisplayFrom = getView().findViewById(R.id.graphs_date_from_date);
+        this.dateDisplayTo = getView().findViewById(R.id.graphs_date_to_date);
         this.applyBtn = getView().findViewById(R.id.graphs_search_button);
+        this.deleteBtn = getView().findViewById(R.id.graphs_delete_button);
     }
 
     private void setupChips() {
@@ -189,6 +175,7 @@ public class GraphsFragment extends Fragment {
                     day.set(datePicker.getDay());
                     dateSelectedFrom = String.format("%04d%02d%02d", datePicker.getYear(), datePicker.getMonth() + 1, datePicker.getDay());
                     this.dateDisplayFrom.setText(String.format("From: %02d/%02d/%04d", datePicker.getDay(), datePicker.getMonth() + 1, datePicker.getYear()));
+                    datePicker.getDataReady().removeObservers(getActivity());
                 }
             });
         });
@@ -209,6 +196,7 @@ public class GraphsFragment extends Fragment {
                     day.set(datePicker.getDay());
                     dateSelectedTo = String.format("%04d%02d%02d", datePicker.getYear(), datePicker.getMonth() + 1, datePicker.getDay());
                     this.dateDisplayTo.setText(String.format("To: %02d/%02d/%04d", datePicker.getDay(), datePicker.getMonth() + 1, datePicker.getYear()));
+                    datePicker.getDataReady().removeObservers(getActivity());
                 }
             });
         });
@@ -217,8 +205,8 @@ public class GraphsFragment extends Fragment {
     private void computeFilters() {
         transactionViewModel.getTransactionList(dateSelectedFrom, dateSelectedTo).observe((LifecycleOwner) getActivity(), transactions -> {
             Log.e("trans", transactions.size() + "");
-            Log.e("from", dateSelectedFrom );
-            Log.e("to", dateSelectedTo );
+            Log.e("from", dateSelectedFrom);
+            Log.e("to", dateSelectedTo);
             generateCharts(transactions);
             transactionViewModel.getTransactionList(dateSelectedFrom, dateSelectedTo).removeObservers((LifecycleOwner) getActivity());
         });
@@ -232,30 +220,34 @@ public class GraphsFragment extends Fragment {
     private void generateCharts(List<Transaction> transactions) {
         AnyChartView lineChartView = view.findViewById(R.id.line_chart);
         lineChartView.setProgressBar(view.findViewById(R.id.line_chart_progress_bar));
+        lineChartView.setVisibility(View.VISIBLE);
         APIlib.getInstance().setActiveAnyChartView(lineChartView);
         //lineChartView.clear();
         // Prepare data
-        HashSet<String> categorySet = new HashSet<>();
+        List<String> categorySet = new ArrayList<>();
         // Retrive Wallet ID's from names (selected)
         List<Integer> walletIDList = new ArrayList<>();
         walletViewModel.getWalletListFromNames(walletSelected).observe((LifecycleOwner) activity, o -> o.forEach(w -> walletIDList.add(w.getId())));
         // Filter the transactions due to the selections of wallets and categories
         transactions.forEach(t -> {
-            if (categorySelected.contains(t.getCategoryName())) {
+            Log.e("TAG", "generateCharts: " + new SimpleDateFormat("dd/MM/yyyy").format(Utilities.getDateFromString(t.getDate())) + " " + t.getCategoryName());
+            if (categorySelected.contains(t.getCategoryName()) && !categorySet.contains(t.getCategoryName())) {
                 categorySet.add(t.getCategoryName());
             }
         });
         // Store all the lists
-        Map<String, List<DataEntry>> lineData = new HashMap<>();
+        List<Pair<String, List<DataEntry>>> lineData = new ArrayList<>();
         categorySet.forEach(c -> {
             List<DataEntry> categoryDataList = new ArrayList<>();
             transactions.forEach(t -> {
                 if (c.equals(t.getCategoryName()) && walletIDList.contains(t.getWalletIdTo())) {
-                    categoryDataList.add(new ValueDataEntry(t.getDate(), t.getAmount()));
+                    categoryDataList.add(new ValueDataEntry(new SimpleDateFormat("dd/MM/yyyy").format(Utilities.getDateFromString(t.getDate())), t.getAmount()));
+                    Log.e("TAG", "generateCharts: " + new SimpleDateFormat("dd/MM/yyyy").format(Utilities.getDateFromString(t.getDate())) + " " + t.getAmount());
                 }
             });
-            lineData.put(c, categoryDataList);
+            lineData.add(new Pair<>(c, categoryDataList));
         });
+
         Chart lineChart = new LineChart(lineChartView, lineData, "Category trendline", "Time", "Amount(€)");
         lineChart.instantiateChart();
 
@@ -263,6 +255,7 @@ public class GraphsFragment extends Fragment {
 
         AnyChartView columnChartView = view.findViewById(R.id.candle_chart);
         columnChartView.setProgressBar(view.findViewById(R.id.candle_chart_progress_bar));
+        columnChartView.setVisibility(View.VISIBLE);
         APIlib.getInstance().setActiveAnyChartView(columnChartView);
 
         // Store all the columns
@@ -270,7 +263,7 @@ public class GraphsFragment extends Fragment {
         categorySet.forEach(c -> {
             AtomicInteger amount = new AtomicInteger(0);
             transactions.forEach(t -> {
-                if (c.equals(t.getCategoryName()) && walletIDList.contains(t.getWalletIdTo())) {
+                if (c.equals(t.getCategoryName()) && walletIDList.contains(t.getWalletIdTo()) && t.getAmount() < 0) {
                     amount.set(amount.intValue() - (int) t.getAmount());
                 }
             });
@@ -281,5 +274,4 @@ public class GraphsFragment extends Fragment {
         columnChart.instantiateChart();
 
     }
-
 }
--- app/src/main/java/com/simoale/debitcredit/ui/graphs/LineChart.java
+++ app/src/main/java/com/simoale/debitcredit/ui/graphs/LineChart.java
@@ -1,24 +1,24 @@
 package com.simoale.debitcredit.ui.graphs;
 
+import android.util.Pair;
+
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.charts.Cartesian;
-import com.anychart.enums.Anchor;
 import com.anychart.enums.MarkerType;
 import com.anychart.enums.TooltipPositionMode;
 
 import java.util.List;
-import java.util.Map;
 
 public class LineChart implements Chart {
 
     AnyChartView anyChartView;
     Cartesian cartesian;
 
-    Map<String, List<DataEntry>> data;
+    List<Pair<String, List<DataEntry>>> data;
 
-    public LineChart(AnyChartView anyChartView, Map<String, List<DataEntry>> data, String title, String xLabel, String yLabel){
+    public LineChart(AnyChartView anyChartView, List<Pair<String, List<DataEntry>>> data, String title, String xLabel, String yLabel) {
         this.anyChartView = anyChartView;
         this.data = data;
         this.cartesian = AnyChart.line();
@@ -28,12 +28,12 @@ public class LineChart implements Chart {
     }
 
     private void setupLines() {
-        this.data.forEach((k, v) -> {
-            cartesian.line(v)
-                    .name(k)
-                    .hovered().markers().enabled(true)
-                    .type(MarkerType.CIRCLE).size(4d);
-            }
+        this.data.forEach(p -> {
+                    cartesian.line(p.second)
+                            .name(p.first)
+                            .hovered().markers().enabled(true)
+                            .type(MarkerType.CIRCLE).size(4d);
+                }
         );
     }
 
