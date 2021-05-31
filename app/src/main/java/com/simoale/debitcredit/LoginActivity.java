7b9acf4646785e57a77f2726fb8386cc18dd76ab
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-05-31 17:06:57 +0200

    Added fingerprint

--- app/src/main/java/com/simoale/debitcredit/LoginActivity.java
+++ app/src/main/java/com/simoale/debitcredit/LoginActivity.java
@@ -3,46 +3,95 @@ package com.simoale.debitcredit;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.simoale.debitcredit.utils.Utilities;
+
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout passwordTextInput;
     private Button loginButton;
 
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_login);
         getSupportActionBar().setTitle("DebitCredit");
+
+
         this.passwordTextInput = findViewById(R.id.login_password_textInput);
         this.loginButton = findViewById(R.id.login_button);
         SharedPreferences preferences = getSharedPreferences("pwd", MODE_PRIVATE);
-
         String password = preferences.getString("password", "-1");
         if (password.equals("-1")) {
             this.handleFirstLogin();
+        } else {
+            this.executeFingerprintLogin();
+            this.loginButton.setOnClickListener(v -> {
+                String val = this.passwordTextInput.getEditText().getText().toString();
+                if (val.equals(password)) {
+                    this.startActivity(new Intent(this, MainActivity.class));
+                } else {
+                    Toast.makeText(this, "Wrong password:" + password + "/", Toast.LENGTH_LONG);
+                }
+            });
         }
+    }
 
-        Log.e("password", password);
+    private void executeFingerprintLogin() {
 
-        String finalPassword = password;
-        this.loginButton.setOnClickListener(v -> {
-            String val = this.passwordTextInput.getEditText().getText().toString();
-            if (val.equals(finalPassword)) {
-                this.startActivity(new Intent(this, MainActivity.class));
-            } else {
-                Toast.makeText(this, "Wrong password:" + finalPassword + "/", Toast.LENGTH_LONG);
-            }
-        });
+
+        executor = ContextCompat.getMainExecutor(this);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+            biometricPrompt = new BiometricPrompt(LoginActivity.this,
+                    executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode,
+                                                  @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Toast.makeText(getApplicationContext(),
+                            "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                            .show();
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(
+                        @NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Toast.makeText(getApplicationContext(),
+                            "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+                    LoginActivity.this.startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Toast.makeText(getApplicationContext(), "Authentication failed",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric login for my app")
+                    .setSubtitle("Log in using your biometric credential")
+                    .setNegativeButtonText("Use account password")
+                    .build();
+            biometricPrompt.authenticate(promptInfo);
+        }
     }
 
     private void handleFirstLogin() {
